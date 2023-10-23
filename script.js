726cd51534dfdf35d95a5a2cb192e7541fc2746c
Roma <nikitinroma2605@gmail.com>
2023-10-23 20:40:22 +0300

    roma's commit

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+const activityLink = document.getElementById("activity-link");
+const activityDropdownBody = document.getElementById("activities-dropdown-body");
 
+document.addEventListener("DOMContentLoaded", function() {
     const activityLink = document.getElementById("activity-link");
     const activityDropdownBody = document.getElementById("activities-dropdown-body");
+  
+    activityLink.addEventListener("click", function(event) {
+        // Toggle the 'active' class to show/hide the dropdown
+      activityDropdownBody.classList.toggle("active");
+    });
+  });
+
 
