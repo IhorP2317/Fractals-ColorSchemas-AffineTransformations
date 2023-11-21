e80dd8697012837e84912ea77c4fdf1cf5aef6c9
Ігор <ihor.paranchuk.pz.2021@lpnu.ua>
2023-11-21 15:33:50 +0200

    code refactored

--- scripts/hslImageSelecting.js
+++ scripts/hslImageSelecting.js
@@ -1,56 +1,33 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // Get references to the canvas and input elements
-  const canvas = document.getElementById("hslCanvas");
+document.getElementById("upload-button").addEventListener("click", function () {
+  document.getElementById("upload-input").click();
+});
+
+document.getElementById("upload-input").addEventListener("change", function () {
+  const fileInput = this;
+  const canvas = document.getElementById("before-canvas");
   const ctx = canvas.getContext("2d");
-  const uploadInput = document.getElementById("upload-input");
 
-  // Add an event listener to the file input
-  uploadInput.addEventListener("change", handleFileSelect);
+  const file = fileInput.files[0];
 
-  function handleFileSelect(event) {
-    const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
 
-    if (file) {
-      // Create a new FileReader
-      const reader = new FileReader();
+    reader.onload = function (e) {
+      const img = new Image();
+      img.onload = function () {
+        // Set canvas size to match the image
+        canvas.width = img.width;
+        canvas.height = img.height;
 
-      // Set the onload callback to update the canvas when the image is loaded
-      reader.onload = function (e) {
-        const img = new Image();
-        img.onload = function () {
-          // Clear the canvas and draw the new image
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-          const aspectRatio = img.width / img.height;
-          const maxWidth = canvas.width;
-          const maxHeight = canvas.height;
+        // Improve image quality
+        ctx.imageSmoothingEnabled = true;
 
-          let newWidth = maxWidth;
-          let newHeight = newWidth / aspectRatio;
-
-          if (newHeight > maxHeight) {
-            newHeight = maxHeight;
-            newWidth = newHeight * aspectRatio;
-          }
-
-          ctx.drawImage(img, 0, 0, newWidth, newHeight);
-
-          ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-        };
-        img.src = e.target.result;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
       };
+      img.src = e.target.result;
+    };
 
-      // Read the selected file as a data URL
-      reader.readAsDataURL(file);
-    }
+    reader.readAsDataURL(file);
   }
-
-  /*function saveImage() {
-        // Add logic to save the canvas content as an image (e.g., toDataURL, etc.)
-        // This can be customized based on your requirements.
-        // Example:
-        const imageData = canvas.toDataURL('image/png');
-        // Now imageData contains the base64 representation of the canvas image.
-        // You can save it or perform further actions as needed.
-        console.log('Image saved:', imageData);
-    }*/
 });
